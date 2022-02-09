2d5629723e966bef21c851a82f20b8d1ca5460b0
AliceColombari <colombari.23@gmail.com>
2022-02-09 13:42:24 +0100

    Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.

---
+++ main.js
@@ -0,0 +1,10 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+var app = new Vue(
+    {
+        el: "#root",
+        data: {
+            message: "Ciao ciao"
+        }
+    }
+);
