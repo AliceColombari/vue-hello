317bf4d1b97edae5a498c96984e9c94c2c117051
AliceColombari <colombari.23@gmail.com>
2022-02-09 13:53:42 +0100

    bonus

---
+++ bonus/main.js
@@ -0,0 +1,10 @@
+//  Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+var app = new Vue(
+    {
+        el: "#root",
+        data: {
+            message: "Vue.js",
+            image: "https://www.geekandjob.com/uploads/wiki/f236a1aa005ef9a93041f294749e86d1.png"
+        }
+    }
+);
